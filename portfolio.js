4ad4d09f0ff800bb45a456ba6dfd16962cc56417
alomac6 <167495912+alomac6@users.noreply.github.com>
2025-04-30 17:44:36 -0500

    finished theme change

--- portfolio.js
+++ portfolio.js
@@ -8,17 +8,42 @@ document.addEventListener('DOMContentLoaded', () => {
     function openMenu() {
         navbar.classList.add('side-menu');
         openButton.style.display='none';
-        closeButton.style.display='block';
+        closeButton.style.display='flex';
         overlay.style.display='block';
     }
     function closeMenu(){
         navbar.classList.remove('side-menu');
-        openButton.style.display='block';
+        openButton.style.display='flex';
         closeButton.style.display='none';
     }
+    const themeButton = document.querySelector('.themeChange');
+    const htmlElement = document.documentElement;
+    function applyTheme(theme) {
+    const lightIcon = document.getElementById('sun');
+    const darkIcon = document.getElementById('eclipse');
+    localStorage.getItem('theme');
+    const validTheme = theme === 'dark' ? 'dark' : 'light';
+    htmlElement.setAttribute('data-theme', validTheme);
+    localStorage.setItem('theme', validTheme);
+         if(validTheme==='dark'){
+            lightIcon.style.display = 'flex';
+            darkIcon.style.display = 'none';
+        }
+        else{
+            lightIcon.style.display = 'none';
+            darkIcon.style.display = 'flex';
+        }
+    }
+    function themeSwitch() {
+        const currentTheme = localStorage.getItem('theme') || 'dark';
+        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        applyTheme(newTheme);
+    }
+    applyTheme(localStorage.getItem('theme'));
+    themeButton.addEventListener('click', themeSwitch);
     openButton.addEventListener('click', openMenu);
     closeButton.addEventListener('click', closeMenu);
     main.addEventListener('click', closeMenu);
     links.addEventListener('click', closeMenu);
-});
+})
 
